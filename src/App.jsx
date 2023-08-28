5def6f5fba7cbc86226a46a715df180c189578b9
ruijosecj <ruijosecj@gmail.com>
2023-08-28 08:16:54 -0300

    commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Formulario from './Components/Formulario';
+
+function App() {
+
+  return (
+    <>
+      <Formulario/>
+    </>
+  )
+}
+
+export default App
